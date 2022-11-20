cb9b599acd32a9b0ab21b3f9e307bb7ef42b81a6
Sudarshan Pandey <94542120+sudarshan57@users.noreply.github.com>
2022-11-20 19:34:54 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,415 @@
+<?php
+
+//To Handle Session Variables on This Page
+session_start();
+
+
+//Including Database Connection From db.php file to avoid rewriting in all files
+require_once("db.php");
+?>
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>Job Portal</title>
+  <!-- Tell the browser to be responsive to screen width -->
+  <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+  <!-- Bootstrap 3.3.7 -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <!-- Ionicons -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="css/AdminLTE.min.css">
+  <link rel="stylesheet" href="css/_all-skins.min.css">
+  <!-- Custom -->
+  <link rel="stylesheet" href="css/custom.css">
+  <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+  <!--[if lt IE 9]>
+  <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+  <![endif]-->
+
+  <!-- Google Font -->
+  <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
+</head>
+<body class="hold-transition skin-green sidebar-mini">
+<div class="wrapper">
+
+  <header class="main-header">
+
+    <!-- Logo -->
+    <a href="index.php" class="logo logo-bg">
+      <!-- mini logo for sidebar mini 50x50 pixels -->
+      <span class="logo-mini"><b>J</b>P</span>
+      <!-- logo for regular state and mobile devices -->
+      <span class="logo-lg"><b>Job</b> Portal</span>
+    </a>
+
+    <!-- Header Navbar: style can be found in header.less -->
+    <nav class="navbar navbar-static-top">
+      <!-- Navbar Right Menu -->
+      <div class="navbar-custom-menu">
+        <ul class="nav navbar-nav">
+          <li>
+            <a href="jobs.php">Jobs</a>
+          </li>
+          <li>
+            <a href="#candidates">Candidates</a>
+          </li>
+          <li>
+            <a href="#company">Company</a>
+          </li>
+          <li>
+            <a href="#about">About Us</a>
+          </li>
+          <?php if(empty($_SESSION['id_user']) && empty($_SESSION['id_company'])) { ?>
+          <li>
+            <a href="login.php">Login</a>
+          </li>
+          <li>
+            <a href="sign-up.php">Sign Up</a>
+          </li>  
+          <?php } else { 
+
+            if(isset($_SESSION['id_user'])) { 
+          ?>        
+          <li>
+            <a href="user/index.php">Dashboard</a>
+          </li>
+          <?php
+          } else if(isset($_SESSION['id_company'])) { 
+          ?>        
+          <li>
+            <a href="company/index.php">Dashboard</a>
+          </li>
+          <?php } ?>
+          <li>
+            <a href="logout.php">Logout</a>
+          </li>
+          <?php } ?>
+        </ul>
+      </div>
+    </nav>
+  </header>
+
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper" style="margin-left: 0px;">
+
+    <section class="content-header bg-main">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12 text-center index-head">
+            <h1>All <strong>JOBS</strong> In One Place</h1>
+            <p>One search, global reach</p>
+            <p><a class="btn btn-success btn-lg" href="jobs.php" role="button">Search Jobs</a></p>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="content-header">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12 latest-job margin-bottom-20">
+            <h1 class="text-center">Latest Jobs</h1>            
+            <?php 
+          /* Show any 4 random job post
+           * 
+           * Store sql query result in $result variable and loop through it if we have any rows
+           * returned from database. $result->num_rows will return total number of rows returned from database.
+          */
+          $sql = "SELECT * FROM job_post Order By Rand() Limit 4";
+          $result = $conn->query($sql);
+          if($result->num_rows > 0) {
+            while($row = $result->fetch_assoc()) 
+            {
+              $sql1 = "SELECT * FROM company WHERE id_company='$row[id_company]'";
+              $result1 = $conn->query($sql1);
+              if($result1->num_rows > 0) {
+                while($row1 = $result1->fetch_assoc()) 
+                {
+             ?>
+            <div class="attachment-block clearfix">
+              <img class="attachment-img" src="img/photo2.jpg" alt="Attachment Image">
+              <div class="attachment-pushed">
+                <h4 class="attachment-heading"><a href="view-job-post.php?id=<?php echo $row['id_jobpost']; ?>"><?php echo $row['jobtitle']; ?></a> <span class="attachment-heading pull-right">$<?php echo $row['maximumsalary']; ?>/Month</span></h4>
+                <div class="attachment-text">
+                    <div><strong><?php echo $row1['companyname']; ?> | <?php echo $row1['city']; ?> | Experience <?php echo $row['experience']; ?> Years</strong></div>
+                </div>
+              </div>
+            </div>
+          <?php
+              }
+            }
+            }
+          }
+          ?>
+
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section id="candidates" class="content-header">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12 text-center latest-job margin-bottom-20">
+            <h1>Candidates</h1>
+            <p>Finding a job just got easier. Create a profile and apply with single mouse click.</p>            
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-sm-4 col-md-4">
+            <div class="thumbnail candidate-img">
+              <img src="img/browse.jpg" alt="Browse Jobs">
+              <div class="caption">
+                <h3 class="text-center">Browse Jobs</h3>
+              </div>
+            </div>
+          </div>
+          <div class="col-sm-4 col-md-4">
+            <div class="thumbnail candidate-img">
+              <img src="img/interviewed.jpeg" alt="Apply & Get Interviewed">
+              <div class="caption">
+                <h3 class="text-center">Apply & Get Interviewed</h3>
+              </div>
+            </div>
+          </div>
+          <div class="col-sm-4 col-md-4">
+            <div class="thumbnail candidate-img">
+              <img src="img/career.jpg" alt="Start A Career">
+              <div class="caption">
+                <h3 class="text-center">Start A Career</h3>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section id="company" class="content-header">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12 text-center latest-job margin-bottom-20">
+            <h1>Companies</h1>
+            <p>Hiring? Register your company for free, browse our talented pool, post and track job applications</p>            
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-sm-4 col-md-4">
+            <div class="thumbnail company-img">
+              <img src="img/postjob.png" alt="Browse Jobs">
+              <div class="caption">
+                <h3 class="text-center">Post A Job</h3>
+              </div>
+            </div>
+          </div>
+          <div class="col-sm-4 col-md-4">
+            <div class="thumbnail company-img">
+              <img src="img/manage.jpg" alt="Apply & Get Interviewed">
+              <div class="caption">
+                <h3 class="text-center">Manage & Track</h3>
+              </div>
+            </div>
+          </div>
+          <div class="col-sm-4 col-md-4">
+            <div class="thumbnail company-img">
+              <img src="img/hire.png" alt="Start A Career">
+              <div class="caption">
+                <h3 class="text-center">Hire</h3>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section id="statistics" class="content-header">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12 text-center latest-job margin-bottom-20">
+            <h1>Our Statistics</h1>
+          </div>
+        </div>
+        <div class="row">
+        <div class="col-lg-3 col-xs-6">
+          <!-- small box -->
+          <div class="small-box bg-aqua">
+            <div class="inner">
+             <?php
+                      $sql = "SELECT * FROM job_post";
+                      $result = $conn->query($sql);
+                      if($result->num_rows > 0) {
+                        $totalno = $result->num_rows;
+                      } else {
+                        $totalno = 0;
+                      }
+                    ?>
+              <h3><?php echo $totalno; ?></h3>
+
+              <p>Job Offers</p>
+            </div>
+            <div class="icon">
+              <i class="ion ion-ios-paper"></i>
+            </div>
+          </div>
+        </div>
+        <!-- ./col -->
+        <div class="col-lg-3 col-xs-6">
+          <!-- small box -->
+          <div class="small-box bg-green">
+            <div class="inner">
+                  <?php
+                      $sql = "SELECT * FROM company WHERE active='1'";
+                      $result = $conn->query($sql);
+                      if($result->num_rows > 0) {
+                        $totalno = $result->num_rows;
+                      } else {
+                        $totalno = 0;
+                      }
+                    ?>
+              <h3><?php echo $totalno; ?></h3>
+
+              <p>Registered Company</p>
+            </div>
+            <div class="icon">
+                <i class="ion ion-briefcase"></i>
+            </div>
+          </div>
+        </div>
+        <!-- ./col -->
+        <div class="col-lg-3 col-xs-6">
+          <!-- small box -->
+          <div class="small-box bg-yellow">
+            <div class="inner">
+             <?php
+                      $sql = "SELECT * FROM users WHERE resume!=''";
+                      $result = $conn->query($sql);
+                      if($result->num_rows > 0) {
+                        $totalno = $result->num_rows;
+                      } else {
+                        $totalno = 0;
+                      }
+                    ?>
+              <h3><?php echo $totalno; ?></h3>
+
+              <p>CV'S/Resume</p>
+            </div>
+            <div class="icon">
+              <i class="ion ion-ios-list"></i>
+            </div>
+          </div>
+        </div>
+        <!-- ./col -->
+        <div class="col-lg-3 col-xs-6">
+          <!-- small box -->
+          <div class="small-box bg-red">
+            <div class="inner">
+               <?php
+                      $sql = "SELECT * FROM users WHERE active='1'";
+                      $result = $conn->query($sql);
+                      if($result->num_rows > 0) {
+                        $totalno = $result->num_rows;
+                      } else {
+                        $totalno = 0;
+                      }
+                    ?>
+              <h3><?php echo $totalno; ?></h3>
+
+              <p>Daily Users</p>
+            </div>
+            <div class="icon">
+              <i class="ion ion-person-stalker"></i>
+            </div>
+          </div>
+        </div>
+        <!-- ./col -->
+      </div>
+      </div>
+    </section>
+
+    <section id="about" class="content-header">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12 text-center latest-job margin-bottom-20">
+            <h1>About US</h1>                      
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-6">
+            <img src="img/browse.jpg" class="img-responsive">
+          </div>
+          <div class="col-md-6 about-text margin-bottom-20">
+          <p>Hello, Dear friends, Welcome to Job Portal also, we are happy you want to know something more about our site</p>
+<p>So, basically, nowadays people are more dependent on online products and services that's why we also, take forward a step to help you.</p>
+<p>Our first wish is to provide you with a better solution to solve your problem. So, kindly if you don't get any solution then mention it in the comment section.</p>
+<p>Also, we are trying to provide fresh & latest content that provides you ideas about all updated information that's happening in the world.</p>
+<p>In the below section you can get more ideas about our site like our website category and content category.</p>
+<p>If you have additional questions or require more information about our About Us Page, do not hesitate to contact us through email at <b>jobportal@gmail.com</b></p>
+<h2><b>What is Our Goal?</b></h2>
+<p>There are millions of websites created every day, also, there is much fake content spread on the internet.</p>
+<p>So, Our main goal is to provide you with 100% Original and Safe content that provides you a great and better experience on the world wide web.</p>
+<p>We mainly focus on our service so and improving it regularly to provide a better user experience to all users.</p>
+<p>Basically, we focus on the job  niche so, our main priority is to search for new content and present it in front of you to learn something new. </p>
+<h3><b>What is our Service?</b></h3>
+<p>We are mainly focused on the job  category so, we provide job  related content if you are interested in the job  category then you can visit daily to get more latest information.</p>
+<p>Job portal is an application which connects employer and job seekers where employers are the source of the resources and the job seeker can find and apply for their targeted job.</p>
+<p>On our website Job Portal you get can all job  related information also, we focus on many other categories and we hope you like also, the content of other categories that are maintained on our website. So, you can visit our website homepage to know all category details here you can visit our homepage Click here --><a href"https://www.jobportal.com">Job Portal</a>.</p>
+<p>Also, we provide a Notification update service you can join by email and other Social Media Platforms, and all Links you can get on the Homepage visit now. <a href"https://www.jobportal.com">Job Portal</a></p>
+<h3><b>About Job Portal</b></h3>
+<p>As you can see already we mention what is our goal and Service again we repeat that we mainly focus on job  Category to help people.</p>
+<p>This Website is Created By Job Portal to help people because many people are still spending hours of time to get exact information so, this is the only motive to create <a href"https://www.jobportal.com">Job Portal</a> to help people and provide them a better web experience. Also, This About Us Page is Generated With <a href="https://soumyahelp.com">Soumya help</a>.</p>
+<p>Now, the time is to know about the Admin Details of this website, so, now we have to go down to know about Admin details.</p>
+<h3><b>Admin's Statement for Job Portal</b></h3>
+<p>As per my point of view, there are many people who visit the internet to get some information but 90% of the time they get wrong information so, the first priority of our website Job Portal is to provide 100% legit and accurate information to our users, Also, I hope my dream comes true one day, and our website will provide Original Content to provide a better user experience. So, From my Side thanks for visiting our website.</p>
+<h3><b>Admin's Contact Information</b></h3>
+<p>Hi, this Sudarshan Pandey, in the above paragraph you know about the website properly and now I am going to provide my contact details.</p>
+<p>If you have any problem & suggestions for this website then you can contact me by using following contact details.</p>
+
+<center>
+    <table><tbody><tr><td>Name</td><strong><td>Sudarshan Pandey</strong></td></tr><tr><td>Email</td><td><strong>jobportal@gmail.com</strong></td></tr><tr><td>Facebook Id</td><td><a href="https://www.facebook.com/sudarshan.pandey.37669/"><strong>jobportal</a></strong></td></tr><tr><td>Twitter Id</td><td><strong><a href="jobportal@gmail.com">@jobportal</a></strong>/</td></tr><tr><td>Instagram Id</td><td><a href="jobportal@gmail.com"><strong>jobportal127</strong></a></td></tr></tbody></table>
+</center>
+
+<p>These details are my personal Account details if you want to contact me then you can contact me by the above platform.</p>
+<p>Finally, this is our complete about us page about details are showing what is the motive to create Job Portal. </p>
+<p>If you want to contact us then you can email us at jobportal@gmail.com</p>
+<center>
+<p>"Thanks for visiting"</p>
+</center>
+  
+          </div>
+        </div>
+      </div>
+    </section>
+
+  </div>
+  <!-- /.content-wrapper -->
+
+  <footer class="main-footer" style="margin-left: 0px;">
+    <div class="text-center">
+      <strong>Copyright &copy; 2022<a href="learningfromscratch.online">Job Portal</a>.</strong> All rights
+    reserved.
+    </div>
+  </footer>
+
+  <!-- /.control-sidebar -->
+  <!-- Add the sidebar's background. This div must be placed
+       immediately after the control sidebar -->
+  <div class="control-sidebar-bg"></div>
+
+</div>
+<!-- ./wrapper -->
+
+<!-- jQuery 3 -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<!-- Bootstrap 3.3.7 -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<!-- AdminLTE App -->
+<script src="js/adminlte.min.js"></script>
+</body>
+</html>
